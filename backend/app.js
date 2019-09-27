523ca3d4976ff2f9cf3a6f505a0db0909a61ed43
Your Name <you@example.com>
2019-09-27 18:46:39 +0300

    Connecting to a database,MongoDb Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
+//MONDODB PASS: yLM22nyFM6MiAaHM
+//MONGODB CONNECTION: mongodb+srv://Annalis_Kirwa:<password>@cluster0-sftiy.mongodb.net/test?retryWrites=true&w=majority
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Annalis_Kirwa:yLM22nyFM6MiAaHM@cluster0-sftiy.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
