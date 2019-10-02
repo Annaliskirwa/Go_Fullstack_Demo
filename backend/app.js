b1ac659bf2ab48925f15031e0e7781bdabd3816c
Your Name <you@example.com>
2019-10-02 16:58:25 +0300

    addede the delete route

--- backend/app.js
+++ backend/app.js
@@ -89,7 +89,23 @@ app.use((req, res, next) => {
       }
     );
   });
-  
+
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
 app.use((req, res, next ) => {
   Thing.find().then(
     (things) => {
